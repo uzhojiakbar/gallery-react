d1452dfde34f9afd7abfcfae3dcf3bc5027c0b67
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-11-04 14:53:34 +0500

    start

---
+++ src/Root/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import { MainRoot } from './style'
+
+const Root = () => {
+    return (
+        <MainRoot>
+            hello
+        </MainRoot>
+    )
+}
+
+export default Root
---
+++ src/Root/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const MainRoot = styled.div`
+  padding: 10px;
+
+  min-width: 100%;
+  min-height: 100vh;
+  background-color: #222222;
+`;
+
+export { MainRoot };
